a196eebe6fa638690c0586f90c31b8b8169161ff
Danilo Mogin <danilo.mogin@gmail.com>
2018-05-23 18:26:05 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment as env } from './../environments/environment';
+
+import { NguCarousel } from '@ngu/carousel';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    title = 'app';
+    screenWidth: number;
+    year = new Date().getFullYear();
+    version = env.version;
+    // logo = require('./../assets/img/Preloader_3.gif');
+    navigation = [
+        { link: 'about', label: 'O Nama', ngClass: '' },
+        { link: 'projects', label: 'Projekti', ngClass: '' },
+        { link: 'retail', label: 'Maloprodaja', ngClass: '' },
+        { link: 'contact', label: 'Kontakt', ngClass: '' }
+    ];
+    navigationSideMenu = [...this.navigation];
+
+    public carouselOne: NguCarousel;
+
+    constructor() {
+        console.log(`App Component!`);
+        console.log(env);
+    }
+    ngOnInit() {
+        this.carouselOne = {
+            grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
+            slide: 1,
+            speed: 400,
+            interval: 4000,
+            point: {
+                visible: true
+            },
+            load: 2,
+            touch: true,
+            loop: true,
+            custom: 'banner'
+        };
+    }
+
+    public myfunc(event: Event) {
+        // carouselLoad will trigger this funnction when your load value reaches
+        // it is helps to load the data by parts to increase the performance of the app
+        // must use feature to all carousel
+        console.log(`myfunc`);
+        console.log(event);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Routes, RouterModule } from '@angular/router';
+import { NguCarouselModule } from '@ngu/carousel';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatSelectModule,
+    MatTabsModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatChipsModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatGridListModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        RouterModule.forRoot([]),
+        NguCarouselModule,
+        FlexLayoutModule,
+
+        MatButtonModule,
+        MatToolbarModule,
+        MatSelectModule,
+        MatTabsModule,
+        MatInputModule,
+        MatProgressSpinnerModule,
+        MatChipsModule,
+        MatCardModule,
+        MatSidenavModule,
+        MatCheckboxModule,
+        MatListModule,
+        MatMenuModule,
+        MatIconModule,
+        MatTooltipModule,
+        MatGridListModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
